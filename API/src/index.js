b81a0a193fa1082a2c863ec5639a66240769ff95
mohamilr <ibrahimdamy7@gmail.com>
2019-06-24 11:34:37 +0100

    commit

--- API/src/index.js
+++ API/src/index.js
@@ -1,1 +1,13 @@
-import express from "express"
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    return res.json({message:"it will work"})
+})
+
+app.listen(port, ()=>{
+    console.log(`app is running on port `);
+})
