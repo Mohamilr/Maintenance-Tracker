4b1393efd560103cdb692b7dfe5d07b881537d19
mohamilr <ibrahimdamy7@gmail.com>
2019-06-27 01:22:08 +0100

    start from the begining

--- API/src/index.js
+++ API/src/index.js
@@ -1,16 +1,12 @@
-// import express
-const express = require('express');
+import express from 'express';
 
-// initialize app
 const app = express();
-
 const port = 3000;
 
-app.get('/', (req, res)=>{
-    return res.json({message:'it will work'})
-})
+app.get('/', (req, res) => {
+  res.json({ message: 'it will work' });
+});
 
-// listen to port
-app.listen(port, ()=>{
-    console.log(`app is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
 })
