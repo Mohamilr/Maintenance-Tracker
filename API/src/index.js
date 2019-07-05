ebb15390e400ad9650c490a47adfb8471ac0aa00
mohamilr <ibrahimdamy7@gmail.com>
2019-07-05 18:56:10 +0100

    get request by id

--- API/src/index.js
+++ API/src/index.js
@@ -1,31 +1,33 @@
-//import express
+// import express
 import express from 'express';
 
-//import body-parser
+// import body-parser
 import bodyParser from 'body-parser';
 
-//get all routes
+// get all routes
 import requestRoutes from './routes/requestRoutes'
 
-//initialize express
+// initialize express
 const app = express();
 
-//create port
+// create port
 const port = 3000;
 
 // configure body-parser
+// app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.json({ extended: true }));
 
-//configure routes
+// configure routes
 app.use('/api/v1/', requestRoutes);
 
 
-//to test if app is running
+// to test if app is running
 app.get('/', (req, res) => {
   res.json({ message: 'it will work' });
 });
 
-//start the express server
+// start the express server
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 })
--- API/src/routes/requestRoutes.js
+++ API/src/routes/requestRoutes.js
@@ -6,7 +6,10 @@ import requestController from '../controller/requestController';
 
 const requestRoute = Router();
 
+//
 requestRoute.get('/users/requests', requestController.allRequests)
+requestRoute.get('/users/requests/:id', requestController.getsingleRequest)
+
 
 //export requestRoute to index.js
 export default requestRoute;
