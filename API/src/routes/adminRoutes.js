afbf1edafe5a04850d5fa1f04e2de755303107fc
mohammed <ibrahimdamy@gmail.com>
2019-08-05 14:04:32 -0700

    disapprove request

--- API/src/routes/adminRoutes.js
+++ API/src/routes/adminRoutes.js
@@ -7,5 +7,6 @@ const adminRoute = Router();
 //all admin routes
 adminRoute.get('/requests/', adminController.getAllRequests);
 adminRoute.put('/requests/:id/approve', adminController.approveARequest);
+adminRoute.put('/requests/:id/disapprove', adminController.disapproveARequest);
 
 export default adminRoute;
