f1cba205d0b2443c9ba4259246e8b512bde86403
mohammed <ibrahimdamy@gmail.com>
2019-08-15 04:59:36 +0100

    connect all endpoints to database

--- API/src/controller/admin.controller.js
+++ API/src/controller/admin.controller.js
@@ -1,67 +1,111 @@
-import requests from '../dummydata/dummyData';
+import jwt from 'jsonwebtoken';
+import pool from '../model/connect.database';
 
 
 const adminController = {
-   getAllRequests(req, res) {
-       res.status(200).json({
+   async getAllRequests(req, res) {
+       try{
+           const allRequestQuery = `SELECT * FROM requests`
+           const requests = await pool.query(allRequestQuery);
+          res.status(200).json({
            message: "all requests",
            count: requests.length,
-           requests
+           requests: requests.rows
        })
+       }
+       catch(err){
+           console.log(err);
+       }   
    },
-   approveARequest(req, res) {
+   async approveARequest(req, res) {
        const id = parseInt(req.params.id);
-       
-       requests.map(request => {
-        requests[id -1] = {
-            id,
-            faultyItem: request.faultyItem,
-            itemType: request.itemType,
-            date: request.date,
-            complaint: request.complaint,
-            status: "Pending"
+       const status = 'Pending';
+
+       try{
+           const approveQuery = `UPDATE requests SET status=$1 WHERE requestId=$2`
+           const values = [status, id];
+           const approveRequest = await pool.query(approveQuery, values)
+        
+           
+           const toApprove = `SELECT * FROM requests WHERE requestId=$1`;
+           const value = [id];
+           const selectedRequest = await pool.query(toApprove, value);
+
+           if(!selectedRequest.rows[0]){
+            res.status(404).json({
+                message: `request with id ${id} is not present in the database`
+            })
         }
-       })
-      
-    res.status(200).json({
-        message: "request approved successfully"
-    })
+           res.status(200).json({
+            message: 'request Approved successfully',
+            approvedRequest: selectedRequest.rows[0]
+          });
+        
+       }
+       catch(err){
+            console.log(err);
+       }
    },
-   disapproveARequest(req, res) {
+   async disapproveARequest(req, res) {
+       const id = parseInt(req.params.id);
+       const status = 'Disapproved';
+
+       try{
+           
+           const disapproveQuery = `UPDATE requests SET status=$1 WHERE requestId=$2`;
+           const values = [status, id];
+           const disapproveRequest = await pool.query(disapproveQuery, values);
+
+           const toDisapprove = `SELECT * FROM requests WHERE requestId=$1`;
+           const value = [id];
+           const selectedRequest = await pool.query(toDisapprove, value);
+
+
+           if(!selectedRequest.rows[0]){
+            res.status(404).json({
+                message: `request with id ${id} is not present in the database`
+            })
+        }
+
+           res.status(200).json({
+               message: 'request Disapproved successfully',
+               disapprovedRequest: selectedRequest.rows[0]
+           });
+       }
+       catch(err){
+           console.log(err);
+       }
+   },
+   async resolveARequest(req, res) {
        const id = parseInt(req.params.id);
 
-       requests.map(request => {
-        requests[id -1] = {
-            id,
-            faultyItem: request.faultyItem,
-            itemType: request.itemType,
-            date: request.date,
-            complaint: request.complaint,
-            status: "Disapproved"
-        }
-       })
-      
-    res.status(200).json({
-        message: "request disapproved successfully"
-    })
-   },
-   resolveARequest(req, res) {
-       const id = parseInt(req.params.id);
+       const status = 'Resolved';
 
-       requests.map(request => {
-        requests[id -1] = {
-            id,
-            faultyItem: request.faultyItem,
-            itemType: request.itemType,
-            date: request.date,
-            complaint: request.complaint,
-            status: "Resolved"
+       try{
+           
+           const resolveQuery = `UPDATE requests SET status=$1 WHERE requestId=$2`;
+           const values = [status, id];
+           const resolveRequest = await pool.query(resolveQuery, values);
+
+           const toResolve = `SELECT * FROM requests WHERE requestId=$1`;
+           const value = [id];
+           const selectedRequest = await pool.query(toResolve, value);
+
+
+           if(!selectedRequest.rows[0]){
+            res.status(404).json({
+                message: `request with id ${id} is not present in the database`
+            })
         }
-       })
-      
-    res.status(200).json({
-        message: "request resolved successfully"
-    })
+
+           res.status(200).json({
+               message: 'request resolved successfully',
+               resolvedRequest: selectedRequest.rows[0]
+           });
+       }
+       catch(err){
+           console.log(err);
+       }
    }
 }
 
--- API/src/controller/sign.auth.controller.js
+++ API/src/controller/sign.auth.controller.js
@@ -1,47 +1,87 @@
 import jwt from 'jsonwebtoken';
-import user from '../dummydata/dummyUser';
+
+import pool from '../model/connect.database';
 
 
 const authenticate = {
-    signUp(req, res) {
-        const id = user[user.length - 1].id;
-        const {username, password} = req.body;
-
-        user.push({
-            id: id + 1,
-            username,
-            password
-        })
-        if(!username || !password){
-            res.status(400).json({
-                message: "username and password required"
-            })
-        }
-        else{
-            jwt.sign({ user }, 'secretkey', { expiresIn: '1h' }, (err, token) => {
-                res.status(200).json({
-                    user,
-                    token
-                })
-            })
-
-        }
-    },
-    logIN(req, res) {
+    async signUp(req, res) {
         const { username, password } = req.body;
 
-        user.forEach(user => {
-            if (username === user.username && password === user.password) {
-                jwt.sign({ username, password }, 'secretkey', { expiresIn: '1h' }, (err, token) => {
-                    res.status(200).json({
-                        token
-                    });
-                });
+        try {
+            const checkUserQuery = `SELECT * FROM users WHERE username=$1`;
+            const value = [username];
+            const check = await pool.query(checkUserQuery, value);
+            if (check.rows[0]) {
+                res.status(400).json({
+                    message: 'username already exist'
+                })
             }
             else {
-                console.log('ERROR, could not get token');
+                const signupQuery = `INSERT INTO users (username, password)
+                VALUES ($1, $2) RETURNING *`;
+                const values = [username, password];
+                const signed = await pool.query(signupQuery, values);
+                res.status(200).json({
+                    user: signed.rows[0]
+                });
             }
-        })
+        }
+        catch (err) {
+            console.log(err);
+        }
+        // if(!username || !password){
+        //     res.status(400).json({
+        //         message: "username and password required"
+        //     })
+        // }
+        // else{
+        //     jwt.sign({ user }, process.env.SECRETKEY, { expiresIn: '1h' }, (err, token) => {
+        //         res.status(200).json({
+        //             user,
+        //             token
+        //         })
+        //     })
+
+        // }
+    },
+    async logIN(req, res) {
+        const { username, password } = req.body;
+
+        try {
+            const logInQuery = `SELECT * FROM users WHERE username=$1`;
+            const value = [username];
+            const user = await pool.query(logInQuery, value);
+            if (process.env.ADMIN_USERNAME === username && process.env.ADMIN_PASSWORD === password) {
+                res.status(200).json({
+                    message: 'user loged in successfully'
+                });
+            }
+            else if (user.rows[0].username === username && user.rows[0].password === password) {
+                res.status(200).json({
+                    message: 'user loged in successfully'
+                });
+
+            }
+        }
+        catch (err) {
+            res.status(403).json({
+                message: 'incorrect username and password'
+            });
+            console.log(err);
+        }
+
+        // user.forEach(user => {
+        //     if (username === user.username && password === user.password) {
+        //         jwt.sign({ username, password }, process.env.SECRETKEY, { expiresIn: '1h' }, (err, token) => {
+        //             res.status(200).json({
+        //                 token
+        //             });
+        //         });
+        //     }
+        //     else {
+        //         console.log('ERROR, could not get token');
+        //     }
+        // })
     },
     verify(req, res, next) {
         const header = req.headers['authorization'];
