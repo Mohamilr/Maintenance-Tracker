5a371b64f84fd5d623c6b4565839a9748e968ece
mohammed <ibrahimdamy@gmail.com>
2019-08-25 01:46:42 +0100

    make changes

--- src/routes/requestRoutes.js
+++ src/routes/requestRoutes.js
@@ -10,9 +10,13 @@ import authenticate from '../controller/sign.auth.controller';
 const requestRoute = Router();
 
 // all request route
-requestRoute.get('/users/requests', authenticate.verify, requestController.allRequest);
+// route to get requests of a loged in user
+requestRoute.get('/users/requests/:id/all', authenticate.verify, requestController.allRequest);
+// route to get a single request
 requestRoute.get('/users/requests/:id', authenticate.verify, requestController.getsingleRequest)
+// route to post a request
 requestRoute.post('/users/requests', authenticate.verify, requestController.addRequest)
+// route to modify a request
 requestRoute.put('/users/requests/:id',authenticate.verify, requestController.modifyARequest)
 
 // export requestRoute to index.js
