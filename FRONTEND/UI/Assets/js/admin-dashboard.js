8a0e92785f92df1ff8810ed7687c212b5e684229
mohammed <ibrahimdamy@gmail.com>
2019-08-25 02:37:16 +0100

    install cors

---
+++ FRONTEND/UI/Assets/js/admin-dashboard.js
@@ -0,0 +1,57 @@
+// parent tag (contents) on the admin dashboard page
+const parentTag = document.querySelector('.contents');
+
+// page discription tag
+const header = document.querySelector('.head');
+
+// generaten token
+const token = localStorage.getItem('token');
+
+// if(!token){
+//   window.location.href = '../sign-in.html';
+// }
+
+// gets all requests for admin function
+const getAllRequests = async () => {
+
+   const response = await fetch('http://localhost:3000/api/v1/requests', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+  .then(res => res.json())
+  .catch(err => { console.log(err) })
+
+ 
+  // if there are no requests in the database
+  if(response.message === 'no request available in the database'){
+      header.textContent = 'NO REQUESTS AVAILABLE IN THE DATABASE';
+  }
+  // else
+  response.requests.forEach(requests => {
+    // format date
+    date = requests.date.substr(0, 10);
+    
+    //  requests format
+     parentTag.innerHTML += `<div db-container>
+       <div tb-headers-admin class="bd-head">
+       <p>${requests.faultyitem}</p>
+       <p>${requests.itemtype}</p>
+       <p>${date}</p>
+       <p class="req-status">${requests.status}</p>
+       </div>
+       <div complaint>
+       <p>Complaint: ${requests.complaint}</p>
+       </div>
+     </div>`; 
+  })
+}
+
+// callback to get all requests
+getAllRequests();
+
+// clears token on logout
+document.querySelector('.logout').addEventListener('click', () => {
+  localStorage.removeItem("token");
+})
---
+++ FRONTEND/UI/Assets/js/login.js
@@ -0,0 +1,65 @@
+
+// for signin
+const logInButton = document.querySelector('.sign-in')
+// form tag
+const form = document.querySelector('.form');
+
+logInButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    // inputs from the form
+    const Username = document.querySelector('#username');
+    const Password = document.querySelector('#password');
+   
+    // inputs values
+    const body = {
+        username: Username.value,
+        password: Password.value
+    }
+
+    // fetch  using promise
+    const response = await fetch('http://localhost:3000/api/v1/auth/login', {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .catch(e => console.log(e))
+
+    // save token in the local storage to be used for authorization
+    localStorage.setItem('token', response.token)
+
+    // alert message
+    const alertMessage = document.createElement('p');
+    alertMessage.style.color = 'red';
+    alertMessage.style.padding = '10px';
+
+    if(response.message === 'incorrect username. please try again or signup if you haven\'t'){
+        alertMessage.textContent = 'incorrect username. please try again or signup if you haven\'t';
+        form.appendChild(alertMessage);
+        
+        // reload page after 3 seconds
+        setTimeout(() =>{
+            window.location = 'sign-in.html';
+        }, 3000);
+    }
+    else if(response.message === 'incorrect username or password'){
+        alertMessage.textContent = 'incorrect username or password';
+        form.appendChild(alertMessage);
+
+        // reload page after 3 seconds
+        setTimeout(() =>{
+            window.location = 'sign-in.html';
+        }, 3000);   
+    }
+    else{
+        // location
+        window.location = '../UI/logedin-admin/dashboard-admin.html';
+    }
+     
+    
+    // empty input fields
+    Username.value = '';
+    Password.value = ''; 
+})
